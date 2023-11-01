19d576d7b12f28870706ee831bd986c1cfd4cb92
KateLogatoc09 <144669024+KateLogatoc09@users.noreply.github.com>
2023-11-01 14:36:47 +0800

    2nd commit
    2nd commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/getData', 'MainController::getData');
+$routes->post('/save', 'MainController::save');
+$routes->post('/del', 'MainController::del');
